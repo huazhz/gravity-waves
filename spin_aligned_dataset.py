9ab029e75835c78906a48a9bd9a2a64c77a989d1
Arnav Das <arnavdas@Arnavs-MacBook-Air.local>
2018-06-17 18:51:28 -0500

    Varied s2z

--- spin_aligned_dataset.py
+++ spin_aligned_dataset.py
@@ -14,20 +14,21 @@ for q in arange(1, 2, 1):
   s1y=0.
   s2x=0.
   s2y=0.
-  s1z=-0.3
-  s2z=-0.9
+  s1z=0.9
+  #s2z=-0.9
 
-  m1, m2 = mtotal_eta_to_mass1_mass2(M, eta)
+  for s2z in arange(0, .9, .1)
+    m1, m2 = mtotal_eta_to_mass1_mass2(M, eta)
 
-  hp, hc = get_td_waveform(approximant='SEOBNRv3', mass1=m1, mass2=m2, spin1x=s1x, spin1y=s1y, spin1z=s1z, spin2x=s2x, spin2y=s2y, spin2z=s2z, f_lower=15., delta_t=1./16384.)
-  waveforms.append( [m1, m2, hp, hc] )
-  print 'generated waveform for q = %f' % q
+    hp, hc = get_td_waveform(approximant='SEOBNRv3', mass1=m1, mass2=m2, spin1x=s1x, spin1y=s1y, spin1z=s1z, spin2x=s2x, spin2y=s2y, spin2z=s2z, f_lower=15., delta_t=1./16384.)
+    waveforms.append( [m1, m2, hp, hc] )
+    print 'generated waveform for q = %f' % q
 
 
-for m1, m2, hp, hc in waveforms:
-  idx = hp.sample_times.data >= 0.
-  pdata = hp.data[idx][0]
-  cdata = hc.data[idx][0]
-  savetxt('q_%.2f_s1z_%.2f_s2z_%.2f_f15.dat' % (m1/m2, s1z, s2z) ,\
-      zip(hp.sample_times.data / (M * 4.92549095e-6), hp.data / sqrt(pdata**2 + cdata**2), hc.data/ sqrt(pdata**2 + cdata**2)), fmt='%.18e\t%.18e\t%.18e')
-  print 'saved waveform for q = %f' % (m1/m2)
+    for m1, m2, hp, hc in waveforms:
+      idx = hp.sample_times.data >= 0.
+      pdata = hp.data[idx][0]
+      cdata = hc.data[idx][0]
+      savetxt('q_%.2f_s1z_%.2f_s2z_%.2f_f15.dat' % (m1/m2, s1z, s2z) ,\
+        zip(hp.sample_times.data / (M * 4.92549095e-6), hp.data / sqrt(pdata**2 + cdata**2), hc.data/ sqrt(pdata**2 + cdata**2)), fmt='%.18e\t%.18e\t%.18e')
+      print 'saved waveform for q = %f' % (m1/m2)
