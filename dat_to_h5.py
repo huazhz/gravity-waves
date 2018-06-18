78d854bf0e67309f3eb6b186845763d92af3065a
Arnav Das <arnavdas@wirelessprv-10-194-113-40.near.illinois.edu>
2018-06-18 11:45:31 -0500

    Added script to convert .dat to h5

---
+++ dat_to_h5.py
@@ -0,0 +1,11 @@
+import numpy as np
+import glob
+import os
+import h5py as h5
+
+with h5.File('data.h5', 'w') as h5f:
+    for f in glob.glob('*.dat'):
+        print f
+        data = np.loadtxt(f)
+        name = f[:-4]
+        h5f[name] = data
