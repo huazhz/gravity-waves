ff4a12c4c05caac08a2b74d55d1d3eeb20502a26
Arnav Das <arnavdas@wirelessprv-10-194-113-40.near.illinois.edu>
2018-06-19 11:49:59 -0500

    Testing code

--- CNN_Predict_s2z.py
+++ CNN_Predict_s2z.py
@@ -3,9 +3,10 @@ import numpy as np
 import random
 import h5py as h5
 import os
-import tensorflow as tf
+#import tensorflow as tf
 
 
+######## DATA PREPROCESSING ########
 def generate_data(filename):
 	f = h5.File(filename)
 	dset = []
@@ -42,76 +43,79 @@ def generate_data(filename):
 
 	return dset, labels
 
+dset, labels = generate_data(train.h5)
+print(dset.shape)
+print(labels.shape)
 
 ######## HELPER FUNCTIONS ########
-def weight(name, shape):
-	return tf.get_variable(name, shape=shape, initializer = tf.contrib.layers.xavier_initializer())
+# def weight(name, shape):
+# 	return tf.get_variable(name, shape=shape, initializer = tf.contrib.layers.xavier_initializer())
 
-def bias(name, shape):
-	return tf.Variable(tf.random_normal([shape]), name=name)
+# def bias(name, shape):
+# 	return tf.Variable(tf.random_normal([shape]), name=name)
 
-def conv(x, W):
-    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding = 'SAME')
+# def conv(x, W):
+#     return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding = 'SAME')
 
-def maxPool(x):
-    return tf.nn.max_pool(x, ksize=[1, 5, 1, 1], strides=[1, 5, 1, 1], padding = 'SAME')
+# def maxPool(x):
+#     return tf.nn.max_pool(x, ksize=[1, 5, 1, 1], strides=[1, 5, 1, 1], padding = 'SAME')
 
 
 ######## CNN MODEL ########
-with tf.name_scope('Input'):
-	X = tf.placeholder(tf.float32, shape = [None, 15000])
+# with tf.name_scope('Input'):
+# 	X = tf.placeholder(tf.float32, shape = [None, 15000])
 
-with tf.name_scope('Labels'):
-	Y = tf.placeholder(tf.float32, shape = [None, 3])
+# with tf.name_scope('Labels'):
+# 	Y = tf.placeholder(tf.float32, shape = [None, 3])
 
-with tf.name_scope('Reshape_input'):
-	inp = tf.reshape(X, [-1, 15000, 1, 1])
+# with tf.name_scope('Reshape_input'):
+# 	inp = tf.reshape(X, [-1, 15000, 1, 1])
 
-with tf.name_scope('Conv1'):
-	w_conv1 = weight('w_conv1', [16, 1, 1, 64])
-	b_conv1 = bias('b_conv1', [64])
-	conv1 = conv(inp, w_conv1) + b_conv1
-	conv1 = tf.nn.relu(conv1)
+# with tf.name_scope('Conv1'):
+# 	w_conv1 = weight('w_conv1', [16, 1, 1, 64])
+# 	b_conv1 = bias('b_conv1', [64])
+# 	conv1 = conv(inp, w_conv1) + b_conv1
+# 	conv1 = tf.nn.relu(conv1)
 
-with tf.name_scope('MaxPool1'):
-	conv1 = maxPool(conv1)
+# with tf.name_scope('MaxPool1'):
+# 	conv1 = maxPool(conv1)
 
-with tf.name_scope('Conv2'):
-	w_conv2 = weight('w_conv2', [16, 1, 64, 128])
-	b_conv2 = bias('b_conv2', [128])
-	conv2 = conv(conv1, w_conv2) + b_conv2
-	conv2 = tf.nn.relu(conv2)
+# with tf.name_scope('Conv2'):
+# 	w_conv2 = weight('w_conv2', [16, 1, 64, 128])
+# 	b_conv2 = bias('b_conv2', [128])
+# 	conv2 = conv(conv1, w_conv2) + b_conv2
+# 	conv2 = tf.nn.relu(conv2)
 
-with tf.name_scope('MaxPool2'):
-	conv2 = maxPool(conv2)
+# with tf.name_scope('MaxPool2'):
+# 	conv2 = maxPool(conv2)
 
-with tf.name_scope('Conv3'):
-	w_conv3 = weight('w_conv3', [16, 1, 128, 256])
-	b_conv3 = bias('b_conv3', [256])
-	conv3 = conv(conv2, w_conv3) + b_conv3
-	conv3 = tf.nn.relu(conv3)
+# with tf.name_scope('Conv3'):
+# 	w_conv3 = weight('w_conv3', [16, 1, 128, 256])
+# 	b_conv3 = bias('b_conv3', [256])
+# 	conv3 = conv(conv2, w_conv3) + b_conv3
+# 	conv3 = tf.nn.relu(conv3)
 
-with tf.name_scope('MaxPool3'):
-	conv3 = maxPool(conv3)
+# with tf.name_scope('MaxPool3'):
+# 	conv3 = maxPool(conv3)
 
-with tf.name_scope('Flatten_layer'):
-	flat = tf.reshape(conv3, [-1, 120 * 256])
+# with tf.name_scope('Flatten_layer'):
+# 	flat = tf.reshape(conv3, [-1, 120 * 256])
 
-with tf.name_scope('FC_layer'):
-	w_fc1 = weight('w_fc1', [120 * 256, 1024])
-	b_fc1 = bias('b_fc1', [1024])
-	fc = tf.matmul(flat, w_fc1) + b_fc1
-	fc = tf.nn.relu(fc)
+# with tf.name_scope('FC_layer'):
+# 	w_fc1 = weight('w_fc1', [120 * 256, 1024])
+# 	b_fc1 = bias('b_fc1', [1024])
+# 	fc = tf.matmul(flat, w_fc1) + b_fc1
+# 	fc = tf.nn.relu(fc)
 
-with tf.name_scope('Output'):
-	w_fc2 = weight('w_f2', [1024, 3])
-	b_fc1 = bias('b_fc2', [3])
-	Y_ = tf.matmul(fc, w_fc2) + b_fc2
+# with tf.name_scope('Output'):
+# 	w_fc2 = weight('w_f2', [1024, 3])
+# 	b_fc1 = bias('b_fc2', [3])
+# 	Y_ = tf.matmul(fc, w_fc2) + b_fc2
 
 
-#Loss function
-cost = tf.losses.mean_squared_error(Y_, Y)
-optimizer = tf.train.AdamOptimizer().minimize(cost)
+# #Loss function
+# cost = tf.losses.mean_squared_error(Y_, Y)
+# optimizer = tf.train.AdamOptimizer().minimize(cost)
 
 
 
