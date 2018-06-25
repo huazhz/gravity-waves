319c476f313f054fbaa5ec24331bc0b58d01ddda
Arnav Das <arnavdas2016@gmail.com>
2018-06-25 17:54:00 -0400

    minor bug

--- CNN_Predict_s2z.py
+++ CNN_Predict_s2z.py
@@ -3,7 +3,7 @@ np.set_printoptions(threshold=np.nan)
 import random
 import h5py as h5
 import tensorflow as tf
-from parse_data.py import *
+from parse_data import *
 
 ######## CNN MODEL ########
 x = tf.placeholder(tf.float32, shape = [None, 30000])
